fa9aa362b79336d8c4cf7be79252d90b0985bf16
mhn <mhnch75@gmail.com>
2024-12-11 06:49:27 +0330

    changes for naya_dongle

--- naya_dongle/board.c
+++ naya_dongle/board.c
@@ -22,31 +22,45 @@ static int board_naya_dongle_init(void)
 	// NRF_CLOCK->LFCLKSRC = CLOCK_LFCLKSRC_SRC_RC;
 	// NRF_CLOCK->TASKS_LFCLKSTART = 1UL;
 
-	// NRF_POWER->DCDCEN0 = 0;
-	// NRF_POWER->DCDCEN = 0;
+	NRF_POWER->DCDCEN0 = 0;
+	NRF_POWER->DCDCEN = 0;
 
-	if ((nrf_power_mainregstatus_get(NRF_POWER) ==
-	     NRF_POWER_MAINREGSTATUS_HIGH) &&
-	    ((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) ==
-	     (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos))) {
 
+	if ((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) != (UICR_REGOUT0_VOUT_3V0 << UICR_REGOUT0_VOUT_Pos)){
 		NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
-		while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
-			;
-		}
-
-		NRF_UICR->REGOUT0 =
-		    (NRF_UICR->REGOUT0 & ~((uint32_t)UICR_REGOUT0_VOUT_Msk)) |
-		    (UICR_REGOUT0_VOUT_3V3 << UICR_REGOUT0_VOUT_Pos);
+		while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+		NRF_UICR->REGOUT0 = (NRF_UICR->REGOUT0 & ~((uint32_t)UICR_REGOUT0_VOUT_Msk)) |
+							(UICR_REGOUT0_VOUT_3V0 << UICR_REGOUT0_VOUT_Pos);
 
 		NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
-		while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
-			;
-		}
+		while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
 
-		/* a reset is required for changes to take effect */
 		NVIC_SystemReset();
 	}
+
+
+	// if ((nrf_power_mainregstatus_get(NRF_POWER) ==
+	//      NRF_POWER_MAINREGSTATUS_HIGH) &&
+	//     ((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) ==
+	//      (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos))) {
+
+	// 	NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+	// 	while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+	// 		;
+	// 	}
+
+	// 	NRF_UICR->REGOUT0 =
+	// 	    (NRF_UICR->REGOUT0 & ~((uint32_t)UICR_REGOUT0_VOUT_Msk)) |
+	// 	    (UICR_REGOUT0_VOUT_1V8 << UICR_REGOUT0_VOUT_Pos);
+
+	// 	NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+	// 	while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+	// 		;
+	// 	}
+
+	// 	/* a reset is required for changes to take effect */
+	// 	NVIC_SystemReset();
+	// }
 
 	return 0;
 }
